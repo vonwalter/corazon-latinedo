4b52babd36ef6db34a22a2c36decba649ab0e491
Walter Carlos Muller <32722615+vonwalter@users.noreply.github.com>
2026-04-26 19:22:16 -0300

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,10 @@
+basic.forever(function () {
+    basic.showIcon(IconNames.Heart)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+})
